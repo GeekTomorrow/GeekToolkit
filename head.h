33b056496463dd5a69333c736347796cf2f1db17
Yeying-Xingchen <yeyingxingchen@yeah.net>
2024-04-20 18:54:40 +0800

    Add MainFiles.

---
+++ head.h
@@ -0,0 +1,11 @@
+﻿// GitToolkit.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#pragma once
+
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+int welcome(int command);
---
+++ index.cpp
@@ -0,0 +1,38 @@
+﻿#include "head.h"
+
+using namespace std;
+
+int main()
+{
+	int language,command;
+	string text;
+
+	cout << "Welcome to GitToolkit, please select your language" << endl<<"1.简体中文"<<endl<<"2.English"<<endl;
+	cin >> language;
+	command = welcome(language);
+
+	if (command == 1)
+	{
+		cin >> text;
+		init(text);
+	}
+	return 0;
+}
+
+int welcome(int command)
+{
+	if (command == 1)
+	{
+		std::cout << "欢迎使用GitToolkit" << endl;
+	}
+	return 1;
+}
+
+int init(string command)
+{
+	if ((command.compare("gITHUB")) == 0)
+	{
+		std::cout << "GitToolkit遇到问题，我们需要重启！" << endl << "请将此错误代码反馈给支持：程序未开发完成！";
+	}
+	return 0;
+}
