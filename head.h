aa13b519661fb5c8fe63aab7087329cfdb311cbc
Yeying-Xingchen <yeyingxingchen@yeah.net>
2024-04-21 17:17:24 +0800

    新增 GITHUB加速。

--- head.h
+++ head.h
@@ -1,3 +1,4 @@
 #pragma once
 #include <iostream>
 #include "versioning/main.h"
+#include "network/main.h"
--- main.cpp
+++ main.cpp
@@ -4,17 +4,23 @@ using namespace std;
 
 int main()
 {
-	int language,command,returnnum;
+	int command;
 	string text;
 	cout << "欢迎使用极客工具箱" << endl<<"1.网站加速"<<endl<<"2.版本管理"<<endl;
 	cin >> command;
 	if (command == 1)
 	{
 		cout<<"网站加速"<<endl<<"1.Github加速（自动化Github520）";
+		cin >> command;
+		if (command == 1)
+		{
+			network_github();
+			cout << "输入数字以退出。";
+		}
 	}
 	if (command == 2)
 	{
-		cout << "版本管理" << endl << "1.初始化仓库" << endl << "2.自动提交代码";
+		cout << "版本管理" << endl << "1.初始化仓库" << endl << "2.自动提交代码" << endl;
 		cin >> command;
 		if (command == 1)
 		{
